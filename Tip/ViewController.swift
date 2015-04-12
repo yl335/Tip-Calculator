e50a9e16581bc74067098abf9f23f0b151e0e2c5
Sara Lin <sara@parastructure.com>
2015-04-11 19:20:45 -0700

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Yanwei Lin on 4/11/15.
+//  Copyright (c) 2015 AppleSeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
