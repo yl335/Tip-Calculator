081e298fd91607b8242409e5b3aa8c18f7bd74cc
Sara Lin <sara@parastructure.com>
2015-04-12 22:30:58 -0700

    Added animation to show/hide tip and total amount depending on whether the bill amount is set or not; Added app icon and launch image; Added Walkthrough GIF and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Tip-Calculator
+Tip-Calculator
+==============
+
+A tip calculator created as the pre-work for CodePath iOS for Designers class. 
+
+## Time Spent
+30 min for basic calculator
+2 - 3 hours refining the design
+4 hours figuring out and debugging the animations and optional features
+
+## User Stories
+* Basic calcuator with 3 tabs
+* Swipe right to clear the bill
+* Added a placeholder '$0' for bill, and prefix '$'
+* When the bill is empty, hide everything else except the bill
+* App icon
+* Launch image
+
+![Video Walkthrough](Walkthrough.gif)
+
+GIF created with (http://www.getcloudapp.com/blog/cloudapp-introduces-version-3 "Cloud App")".
--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billContainer: UIView!
+    @IBOutlet weak var billLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let maxBillContainerHeight: CGFloat = 350.0
+    let minBillContainerHeight: CGFloat = 168.0
+    let minBillLabelY: CGFloat = 74.0
+    let maxBillLabelY: CGFloat = 175.0
+    let minBillFieldY: CGFloat = 64.0
+    let maxBillFieldY: CGFloat = 165.0
+    let minBillLabelFontSize: CGFloat = 14.0
+    let maxBillLabelFontSize: CGFloat = 18.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +39,12 @@ class ViewController: UIViewController {
         // Initial values for labels
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        
+        // Hide tip and total labels
+        billContainer.frame.size.height = self.maxBillContainerHeight
+        tipControl.alpha = 0
+        billLabel.frame.origin.y = maxBillLabelY
+        billField.frame.origin.y = maxBillFieldY
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +63,8 @@ class ViewController: UIViewController {
         var billString = billField.text
         if billString.hasPrefix("$") {
             billString = billString.substringFromIndex(advance(billString.startIndex, 1))
+            
+            // Set field to empty if only '$' remains
             if billString.isEmpty {
                 billField.text = nil
             }
@@ -62,10 +81,42 @@ class ViewController: UIViewController {
         // Update values
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        UIView.animateKeyframesWithDuration(1.0, delay: 0.5, options: nil, animations: {
+            // If no bill, maximize screen for bill input
+            if (billAmount.isZero) {
+                
+                UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.25, animations: {
+                    self.tipControl.alpha = 0
+                })
+
+                UIView.addKeyframeWithRelativeStartTime(0.3, relativeDuration: 0.7, animations: {
+                    self.billContainer.frame.size.height = self.maxBillContainerHeight
+                    self.billLabel.frame.origin.y = self.maxBillLabelY
+                    self.billField.frame.origin.y = self.maxBillFieldY
+                })
+            }
+            // If there is a bill, show tip & total
+            else {
+                
+                UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.7, animations: {
+                    self.billContainer.frame.size.height = self.minBillContainerHeight
+                    self.billLabel.frame.origin.y = self.minBillLabelY
+                    self.billField.frame.origin.y = self.minBillFieldY
+                })
+                
+                UIView.addKeyframeWithRelativeStartTime(0.75, relativeDuration: 0.25, animations: {
+                    self.tipControl.alpha = 1
+                })
+            }
+            
+        }, completion: { finished in
+        })
     }
     
     @IBAction func onSwipeBill(sender: AnyObject) {
-        billField.text = nil
+        billField.text = "$"
+        onEditingChanged(sender)
     }
 }
 
